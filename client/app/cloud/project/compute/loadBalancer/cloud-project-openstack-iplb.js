749a5a6ee6d6d8ec7c6a433e41d120cde6245b77
Ludovic Lamarche <ludovic.lamarche@corp.ovh.com>
2017-10-18 17:36:50 +0200

    first draft new version CX

--- client/app/cloud/project/compute/loadBalancer/cloud-project-openstack-iplb.js
+++ client/app/cloud/project/compute/loadBalancer/cloud-project-openstack-iplb.js
@@ -3,12 +3,12 @@
 angular.module("managerApp")
       .config(function ($stateProvider) {
           $stateProvider
-          .state("iaas.pci-project.openstack.iplb", {
+          .state("iaas.pci-project.compute.iplb.old", {
               url: "/ipLoadbalancing?validate",
               sticky: true,
               views: {
                 cloudProjectOpenstack: {
-                    templateUrl: "app/cloud/project/openstack/iplb/cloud-project-openstack-iplb.html",
+                    templateUrl: "app/cloud/project/compute/loadBalancer/cloud-project-openstack-iplb.html",
                     controller: "CloudProjectOpenstackIplbCtrl",
                     controllerAs: "CloudProjectOpenstackIplbCtrl"
                 }
